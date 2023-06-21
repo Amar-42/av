681895a69da0eea774dd3ea65cf47c2eed93f65c
Amar <133847903+Amar-42@users.noreply.github.com>
2023-06-21 15:49:15 +0530

    Add files via upload
    website

---
+++ play.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+    @include('header')
+<div class="row">
+  <div class="col-md-6">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel"  >
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/slide1.jpg" class="d-block w-100" alt="img1">
+        </div>
+        <div class="carousel-item">
+          <img src="img/slide2.jpg" class="d-block w-100" alt="img2">
+        </div>
+        <div class="carousel-item">
+          <img src="img/slide3.jpg" class="d-block w-100" alt="img3">
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="col-md-6">
+    <div class="container">
+     <h2>शालेय शिक्षण व क्रीडा विभाग</h2>
+     <p>खऱ्या अर्थाने कोणी कोणाला शिकवू शकत नाही. एखाद्या रोपटयाचा जसा आपोआपच विकास होतो तसाच नैसर्गिक अडथळे दूर करणे, आणि त्याच्या शक्तींना जागृती आणणे. योग्य खतपाणी घातले, प्रकाश व हवापाणी यांची सोय केली, सभोवती कुंपन घातले, म्हणजे रोपटे सुरक्षित राहील व त्याचा योग्य प्रकारे विकास होईल. तसेच मुलांचेही आहे. म्हणून विकासाला योग्य‍ परिस्थिती निमार्ण करुन देणे हेच शिक्षकाचे कार्य. या दृष्टीने शिक्षण हे अभावात्मक म्हणावे लागेल.<br><i style="margin-left: 490px;">- स्वामी विवेकानंद</i></p>
+     <div class="card" style="width:400px; height: 500px;">
+       <img class="card-img-top" src="img/Swami.jpg" alt="Card image" style="width:60%; margin-left: 50px; padding-top: 9px;">
+       <div class="card-body">
+         <h4 class="card-title">Swami Vivekananda</h4>
+         <p class="card-text">स्वामी विवेकानंद (१२ जानेवारी, १८६३ - ४ जुलै, १९०२, नरेंद्रनाथ दत्त) हे एक भारतीय संन्यासी आणि तत्त्वज्ञ होते.</p>
+         <a href="https://mr.wikipedia.org/wiki/%E0%A4%B8%E0%A5%8D%E0%A4%B5%E0%A4%BE%E0%A4%AE%E0%A5%80_%E0%A4%B5%E0%A4%BF%E0%A4%B5%E0%A5%87%E0%A4%95%E0%A4%BE%E0%A4%A8%E0%A4%82%E0%A4%A6" class="btn btn-primary stretched-link">अधिक माहिती</a>
+       </div>
+     </div>
+   </div>
+ </div>
+</div><br><br>
+<!--
+<footer>
+  <div>
+   <a href="https://education.maharashtra.gov.in/" class="btn btn-primary">Maharashtra Goverment</a> 
+ </div>
+</footer>
+-->
+<hr>
+@include('footer')
+</body>
+</html>
